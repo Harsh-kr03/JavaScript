ba34984a49b64593e5000a60237cf36fae82eb39
Harsh-kr03 <143214895+Harsh-kr03@users.noreply.github.com>
2024-04-01 06:11:54 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,6 +3,8 @@ let userEmail = "harsh@code.com"
 var userCity = "jaipur"
 userPassword = "124134"
 
+let userState; // on  only declaration js gives undefined as output
+
 // useId = 5  // not allowed bcz of const
 
 
@@ -12,7 +14,7 @@ userPassword = "1124"
 
 console.log(userId); // can only print one thing at a time
 
-console.table([userId, userEmail, userCity, userPassword]);
+console.table([userId, userEmail, userCity, userPassword, userState]);
 
 /*
 Prefer not to use var
