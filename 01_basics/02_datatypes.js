bad7d7cf62337e62fd731a761c7ad2a6551d3a67
Harsh-kr03 <143214895+Harsh-kr03@users.noreply.github.com>
2024-04-01 06:39:35 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all js code as newer version of js
+
+
+// alert(3+5) // we are using nodejs, not browser
+
+console.log(3 + 
+    8);   // this will work but code readability should be high
+    
+    let name = "Harsh"
+    let age = 19
+    let isLoggedIn = true
+    let state;
+    //variables in js
+
+    // number => 2 to power 53(range)
+    // bigint => used for nuber greater than number(datatype)
+    //  string => ""
+    // boolean => true/false
+    // null => standalone value(empty value)
+    // undefined
+    // symbol => unique
+
+    // object
+
+    console.table([typeof undefined, typeof null, typeof "", typeof state, typeof age ]);
