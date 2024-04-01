c6ac7ddc331674dc0b3ff357ab47817eafea07bf
Harsh-kr03 <143214895+Harsh-kr03@users.noreply.github.com>
2024-04-01 06:03:14 +0000

    conatiner config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const userId = 10;
+let userEmail = "harsh@code.com"
+var userCity = "jaipur"
+userPassword = "124134"
+
+// useId = 5  // not allowed bcz of const
+
+
+userEmail = "abc@google.com"
+userCity = "delhi"
+userPassword = "1124"
+
+console.log(userId); // can only print one thing at a time
+
+console.table([userId, userEmail, userCity, userPassword]);
+
