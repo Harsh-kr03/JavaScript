874d54e57de7715258c291cfdd7c2a3b9ac9f21e
Harsh-kr03 <143214895+Harsh-kr03@users.noreply.github.com>
2024-04-01 07:56:58 +0000

    conversion of datatypes

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,46 @@
+//  let score = 89
+ let score = "harsh"
+// let score = true
+// let score = "78abc"
+
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+ let valInNumber = Number(score)
+//  console.log(typeof valInNumber);
+//  console.log(valInNumber);
+
+ //"89" => 89
+ //"78abc" => NaN
+//  true => 1 ; false => 0
+// "harsh" => NaN
+
+
+// let isLoggedIn = "harsh"
+let isLoggedIn = 8
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 or any number => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let value = null
+
+let stringValue = String(value);
+console.log(stringValue);
+console.log(typeof stringValue);
+
+// 89 => string
+// true / 1 =>string 
+
+
+
+
+
+
+
+
